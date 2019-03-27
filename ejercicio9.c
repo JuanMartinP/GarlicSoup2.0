43979217bf9bc1577e3fdee78efbdfdee50b0b58
CodeNotAcceptable <36109227+code406@users.noreply.github.com>
2019-03-27 23:00:22 +0100

    Less of a freestyle but still pretty bad

--- ejercicio9.c
+++ ejercicio9.c
@@ -1,88 +1,119 @@
+#include <fcntl.h>
+#include <semaphore.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
-#include <time.h>
-#include <semaphore.h>
-#include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <time.h>
 #include <unistd.h>
 
 #define N_PROC 5
+#define SEM "/recurso"
+#define RANDMAX 100000
+#define WIN 1234
 
-int main(){
+/* manejador_SIGUSR1 */
+void manejador_SIGUSR1(int sig) {}
 
-    pid_t pid;
-    pid_t ppid;
-    FILE *salida;
-    int idproc, i, ganador, temp;
-    int contador[N_PROC];
+int main() {
+  pid_t pid;
+  FILE *salida = NULL;
+  sem_t *sem = NULL;
+  int i, ganador = N_PROC, temp = 0, flag = 0, r;
+  int contador[N_PROC] = {0};
+  struct sigaction act;
 
-    temp = 0;
-    meta = 0;
-    idproc = N_PROC++; /*Para que no este en el padre sin inicializar*/
-    ppid = getpid();
+  /* Opening the semaphore */
+  if ((sem = sem_open(SEM, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, 1)) ==
+      SEM_FAILED) {
+    perror("sem_open");
+    return -1;
+  }
 
-    for(i=0; i <= N_PROC; i++)   /*Inicializo todo contador a 0 (?)*/
-        contador[i] = 0;
+  /* Para que se vacie el archivo */
+  salida = fopen("salida.txt", "w");
+  fclose(salida);
 
-    /*TODO manejadores*/
+  /* Se arma la señal SIGUSR1 */
+  act.sa_handler = manejador_SIGUSR1;
+  if (sigaction(SIGUSR1, &act, NULL) < 0) {
+    perror("sigaction");
+    exit(EXIT_FAILURE);
+  }
 
-    for (i  = 0; i < N_PROC; i++) {
+	printf("PREPARADOS?\n");
+	sleep(1);
+	printf("LISTOS?\n\n");
+	sleep(1);
 
-        pid = fork();
+  for (i = 0; i < N_PROC; i++) {
+    pid = fork();
+    if (pid < 0) {
+      perror("fork");
+      exit(EXIT_FAILURE);
+    }
+    /*PROCESO HIJO. Sensible a SIGTERM y SIGUSR1*/
+    else if (pid == 0) {
+      srand(getpid());
+			r = rand();
+			printf("Proceso %d: Listo para la carrera\n", i);
+      /* Espera al SIGUSR1 (salida del padre) para una carrera + o - justa */
+      pause();
+      /* Dentro de un bucle, cada participante escribe en un fichero su id
+      y duerme un tiempo aleatorio de máximo una décima. */
+      while (1) {
+        sem_wait(sem);
+        /* Abro fichero para escribir ("a": anadir sin sobreescribir) */
+        salida = fopen("salida.txt", "a");
+        fprintf(salida, "%d ", i);
+        fclose(salida);
+        sem_post(sem);
+        /* DUERME MAXIMO UNA DECIMA: RANDOMIZAR */
+        usleep((r % RANDMAX)+1);
+      }
+      sem_close(sem);
+      exit(EXIT_SUCCESS);
+    }
+  }
 
-        if(pid < 0) {
+	/* Padre da la salida (SIGUSR1) */
+	sleep(2);
+	printf("\nYA!\n\n");
+  kill(0, SIGUSR1);
 
-            perror("fork");
-            exit(EXIT_FAILURE);
+  /*El padre lee el fichero contando cada numero y guardandolo donde
+   * corresponde. Al llegar a 20 anota el ganador*/
+  while (ganador >= N_PROC) {
+    sleep(1);
+    sem_wait(sem);
 
-        }/*PROCESO HIJO*/
-        else if(pid == 0) {
+	  /* Padre abre fichero para lectura */
+	  salida = fopen("salida.txt", "r");
 
-            idproc = i;       /*NO ESTOY SEGURO DE QUE NO SOBREESCRIBA LA VARIABLE A CADA ITERACION DEL BUCLE*/
-
-            /*Envio una señal al padre para avisar de que ya estoy listo*/
-            kill(ppid, SIGUSR1);
-            /*Hago pause() para que todos los procesos empiecen a la vez y sea una "carrera justa"*/
-            pause();
-
-            while(1){
-                /*Abro fichero para escribir, modo "a" para anadir y no sobreescribir*/
-                salida = fopen("salida.txt", "a");
-                fprintf(salida, "%d", idproc);
-                /*Se deberia salir de aqui con el sigterm que lanza el padre*/
-            }
-
-
-        }/*PROCESO PADRE*/
-        else{
-            /*Padre espera a que le llegue la señal a sus hijos*/
-            pause();
+    while (fscanf(salida, "%d", &temp) && flag != WIN && temp!=EOF) {
+      contador[temp]++;
+      for (i = 0; i < N_PROC; i++) {
+        if (contador[i] >= 20) {
+          ganador = i;
+          flag = WIN;
+					signal(SIGTERM, SIG_IGN);
+          kill(0, SIGTERM); /* we done, kill the competitors */
         }
-
+      }
     }
+    sem_post(sem);
+	  fclose(salida);
+  }
+  sem_close(sem);
+  sem_unlink(SEM);
+	for (i=0; i<N_PROC; i++){
+		printf("Proceso %d: %d escrituras\n", i, contador[i]);
+	}
+	printf("\nHa GANADO el %d!! Escribio %d veces\n", ganador, contador[ganador]);
+  /* El padre espera a que todos sus hijos finalicen */
+  for (i = 0; i < N_PROC; i++)
+    wait(NULL);
 
-    /*Segun el ejercicio 4(linea 68) aquí se ejecuta el padre tras recibir la señal de todos sus hijos*/
-    salida = fopen("salida.txt", "r");
-
-/*En este bucle el padre lee el fichero contando cada numero y guardandolo donde corresponde al llegar a 20 anota el ganador, hago los comentarios tan largos para que no se te pase ninguna explicacion <3*/
-    while(ganador > 4){
-        do{
-            fscanf(salida, "%d", temp);
-            for(i = 0; i < N_PROC; i++){
-                if(temp == i)
-                    contador[i]++;
-
-                if(contador[i] >= 20)
-                    ganador = i;
-            }
-        }while(temp != null)
-        sleep(1);
-    }
-
-    for(i = 0; i < N_PROC; i++)
-        wait();
-
-    exit(EXIT_SUCCESS);
+  exit(EXIT_SUCCESS);
 }
